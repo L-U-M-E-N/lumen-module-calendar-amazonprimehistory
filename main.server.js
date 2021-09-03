456d21ba7707b2659ef87a768f25f3980bb781ff
Elanis <elanis@hotmail.com>
2021-09-03 20:03:14 +0200

    Copied Amazon history importer code from old lumen server

---
+++ main.server.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+
+const FILENAME = path.join(process.cwd(), 'res/amazon/Digital.PrimeVideo.ViewingHistory.csv');
+
+module.exports = class AmazonCalendar {
+	static init() {
+		AmazonCalendar.importAmazonPrimeVideoActivity();
+
+		const watcher = fs.watch(FILENAME, { persistent: false }, (curr, prev) => {
+			watcher.close();
+
+			log('./reloading.STATS.AmazonPrime', 'boot');
+
+			AmazonCalendar.importAmazonPrimeVideoActivity();
+		});
+	}
+
+	static importAmazonPrimeVideoActivity() {
+		const file = fs.readFileSync(FILENAME).toString();
+
+		let firstLineSkipped = false;
+		for(const line of file.split('\n')) {
+			if(!firstLineSkipped || line === '') {
+				firstLineSkipped = true;
+				continue;
+			}
+
+			const elements = line.split(',');
+
+			const startElements = elements[0].split(' ');
+			const startDateElements = startElements[0].split('/');
+			const start = new Date(`${startDateElements[2]}-${startDateElements[0]}-${startDateElements[1]}T${startElements[1]}Z`);
+			const end = new Date(start);
+			end.setHours(end.getHours() + 1);
+
+			const id = 'PrimeVideo-' + start;
+
+			const field = {
+				id: id,
+				title: 'PrimeVideo',
+				description: elements[12],
+				start,
+				end,
+				origin: ''
+			};
+
+			//console.log(field);
+
+			const [query, values] = Database.buildInsertQuery('calendar', field);
+
+			Database.execQuery(
+				query,
+				values
+			);
+		}
+
+		log('Saved Amazon Activity', 'info');
+	}
+};
